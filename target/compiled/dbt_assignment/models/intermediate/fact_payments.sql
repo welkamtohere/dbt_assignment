700cb79be19057cbc8dc94f422f3193b2acd1cc6
giwang.kintan <giwang.kintan@shopee.com>
2026-09-13 15:59:19 +0700

    feat: udpate script and docs

---
+++ target/compiled/dbt_assignment/models/intermediate/fact_payments.sql
@@ -0,0 +1,42 @@
+with payments as (
+
+    select * from "analytics"."dbt_assignment_assignment"."stg_payments"
+
+),
+
+customers as (
+
+    select customer_id, full_name from "analytics"."dbt_assignment_assignment"."stg_customers"
+
+),
+
+rentals as (
+`
+    select 
+        r.rental_id, 
+        i.film_id, 
+        i.store_id
+    from "analytics"."dbt_assignment_assignment"."stg_rentals" r
+    left join "analytics"."dbt_assignment_assignment"."stg_inventory" i on i.inventory_id = r.inventory_id
+
+),
+films as (
+    select film_id, title from "analytics"."dbt_assignment_assignment"."stg_films"
+)
+
+selec
+    p.payment_id, 
+    p.paid_at, 
+    p.paid_date, 
+    p.customer_id, 
+    c.full_name as customer_name, 
+    p.staff_id, 
+    r.store_id, 
+    p.rental_id, 
+    f.film_id, 
+    f.title as film_title, 
+    p.amount
+from payments p
+left join customers c on c.customer_id = p.customer_id
+left join rentals r on r.rental_id = p.rental_id
+left join films f on f.film_id = r.film_id
